e5d5b3d51d7ef11f6f250f271d2f9254cf11488f
Prince Kongo <pkkongo@st.ug.edu.gh>
2023-08-22 13:50:18 -0700

    made changes to printf file

--- _printf.c
+++ _printf.c
@@ -1,66 +1,64 @@
-#include "main.h" 
- 
-void print_buffer(char buffer[], int *buff_ind); 
- 
-/** 
- * _printf - Printf function 
- * @format: format. 
- * Return: Printed chars. 
- */ 
-int _printf(const char *format, ...) 
-{ 
- int i, printed = 0, printed_chars = 0; 
- int flags, width, precision, size, buff_ind = 0; 
- va_list list; 
- char buffer[BUFF_SIZE]; 
- 
- if (format == NULL) 
-  return (-1); 
- 
- va_start(list, format); 
- 
- for (i = 0; format && format[i] != '\0'; i++) 
- { 
-  if (format[i] != '%') 
-  { 
-   buffer[buff_ind++] = format[i]; 
-   if (buff_ind == BUFF_SIZE) 
-    print_buffer(buffer, &buff_ind); 
-   /* write(1, &format[i], 1);*/ 
-   printed_chars++; 
-  } 
-  else 
-  { 
-   print_buffer(buffer, &buff_ind); 
-   flags = get_flags(format, &i); 
-   width = get_width(format, &i, list); 
-   precision = get_precision(format, &i, list); 
-   size = get_size(format, &i); 
-   ++i; 
-   printed = handle_print(format, &i, list, buffer, 
-    flags, width, precision, size); 
-   if (printed == -1) 
-    return (-1); 
-   printed_chars += printed; 
-  } 
- } 
- 
- print_buffer(buffer, &buff_ind); 
- 
- va_end(list); 
- 
- return (printed_chars); 
-} 
- 
-/** 
- * print_buffer - Prints the contents of the buffer if it exist 
- * @buffer: Array of chars 
- * @buff_ind: Index at which to add next char, represents the length. 
- */ 
-void print_buffer(char buffer[], int *buff_ind) 
-{ 
- if (*buff_ind > 0) 
-  write(1, &buffer[0], *buff_ind); 
- 
- *buff_ind = 0; 
-} 
+#include <main.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * _putchar - Writes a character to stdout.
+ * @c: The character to be written.
+ * Return: On success, returns the character written. On error, returns -1.
+ */
+int _putchar(char c) {
+    return write(1, &c, 1);
+}
+
+/**
+ * _printf - Custom printf function that handles 'c', 's', and '%'.
+ * @format: A format string with optional conversion specifiers.
+ * Return: The number of characters printed (excluding the null byte).
+ */
+int _printf(const char *format, ...) {
+    va_list args;
+    int count = 0;
+
+    va_start(args, format);
+
+    while (format && *format) {
+        if (*format != '%') {
+            // Regular character, print it to stdout using _putchar
+            _putchar(*format);
+            count++;
+        } else {
+            format++; // Move past '%'
+            if (*format == 'c') {
+                // Character specifier
+                char c = va_arg(args, int); // Get the character argument
+                _putchar(c);
+                count++;
+            } else if (*format == 's') {
+                // String specifier
+                char *str = va_arg(args, char *); // Get the string argument
+                if (str == NULL)
+                    str = "(null)";
+                while (*str) {
+                    _putchar(*str);
+                    str++;
+                    count++;
+                }
+            } else if (*format == '%') {
+                // Percentage sign
+                _putchar('%');
+                count++;
+            } else {
+                // Unsupported specifier, just print the '%'
+                _putchar('%');
+                count++;
+            }
+        }
+        format++;
+    }
+
+    va_end(args);
+
+    return count;
+}
+
